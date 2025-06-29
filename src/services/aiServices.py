4891535b8102cce43dd4dbe9e0cd416921daaed7
IvanRiumkin <ivri0001@gmail.com>
2025-06-29 12:25:34 +0200

    Adjusted story prompt to remove markups

--- src/services/aiServices.py
+++ src/services/aiServices.py
@@ -28,7 +28,8 @@ def get_short_story_about_location(lat: float, long: float):
                   "Aim to adjust the text to the target audience of people who are tourists or history enthusiasts in "
                   "the area and would like to learn more about the history of this location",
                   "if the location is unknown to you say something witty. Do your best to remember what notable "
-                  "people were born in the location or what notable events happened there!"],
+                  "people were born in the location or what notable events happened there! DO NOT USE markup signs "
+                  "such as ** and any others."],
     )
     return genai_resul.text
 
